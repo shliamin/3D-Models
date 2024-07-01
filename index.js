736cbf897d0911112171ab7c304ce5e0decdf884
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-01 20:41:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { generateModel } from './server.js';
 import { generatePattern } from './server.js';
 
 document.getElementById('generateModelButton').onclick = generateModel;
+document.getElementById('generatePatternButton').onclick = generatePattern;
 document.getElementById('width').onchange = updateModel;
 document.getElementById('depth').onchange = updateModel;
 document.getElementById('height').onchange = updateModel;
