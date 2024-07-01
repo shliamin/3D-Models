a96b9bf2d3f22d0a3f4cdd75a32d005c81b38c5a
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-01 18:21:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import { updateModel } from './model.js';
 import { generateModel } from './server.js';
 
 document.getElementById('generateModelButton').onclick = generateModel;
+document.getElementById('width').onchange = updateModel;
+document.getElementById('depth').onchange = updateModel;
+document.getElementById('height').onchange = updateModel;
+document.getElementById('surface1').onchange = updateModel;
+document.getElementById('surface2').onchange = updateModel;
 
-
+// Initialize the model on page load
 window.onload = updateModel;
