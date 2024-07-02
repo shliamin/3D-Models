cf4c09dfaeba5c25208191452a59fb876ff1ae7d
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-02 03:30:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ export async function generateModel() {
 
     console.log('Current values for 3D Model:', { width, depth, height });
 
-    const surface1 = interpolateSurface(circularArc([0, 0], [width, depth], height), circularArc([width, 0], [0, depth], height));
-    const surface2 = interpolateSurface(circularArc([0, 0], [width, depth], height), circularArc([0, depth], [width, 0], height));
+    const arc1 = circularArc([0, 0], [width, depth], height);
+    const arc2 = circularArc([width, 0], [0, depth], height);
+
+    const surface1 = interpolateSurface(arc1, arc2);
+    const surface2 = interpolateSurface(arc1, arc2);
 
     const payload = {
         surface1,
-        surface2
+        surface2,
+        arcs: {
+            arc1,
+            arc2
+        }
     };
 
     try {
@@ -47,12 +54,19 @@ export async function generatePattern() {
 
     console.log('Current values for 2D Pattern:', { width, depth, height });
 
-    const surface1 = interpolateSurface(circularArc([0, 0], [width, depth], height), circularArc([width, 0], [0, depth], height));
-    const surface2 = interpolateSurface(circularArc([0, 0], [width, depth], height), circularArc([0, depth], [width, 0], height));
+    const arc1 = circularArc([0, 0], [width, depth], height);
+    const arc2 = circularArc([width, 0], [0, depth], height);
+
+    const surface1 = interpolateSurface(arc1, arc2);
+    const surface2 = interpolateSurface(arc1, arc2);
 
     const payload = {
         surface1,
-        surface2
+        surface2,
+        arcs: {
+            arc1,
+            arc2
+        }
     };
 
     try {
