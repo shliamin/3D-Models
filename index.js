af9218f51cc851775a5744538b3e3006b242724c
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-05 02:13:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,33 @@ import { updateModel } from './model.js';
 import { generateModel } from './server.js';
 import { generatePattern } from './server.js';
 
-document.getElementById('generateModelButton').onclick = generateModel;
-document.getElementById('generatePatternButton').onclick = generatePattern;
-document.getElementById('width').onchange = updateModel;
-document.getElementById('depth').onchange = updateModel;
-document.getElementById('height').onchange = updateModel;
-document.getElementById('surface1').onchange = updateModel;
-document.getElementById('surface2').onchange = updateModel;
+document.addEventListener('DOMContentLoaded', () => {
+    // Установка обработчиков событий после полной загрузки DOM
+    document.getElementById('generateModelButton').onclick = generateModel;
+    document.getElementById('generatePatternButton').onclick = generatePattern;
+    document.getElementById('width').onchange = updateModel;
+    document.getElementById('depth').onchange = updateModel;
+    document.getElementById('height').onchange = updateModel;
+    document.getElementById('surface1').onchange = updateModel;
+    document.getElementById('surface2').onchange = updateModel;
 
-document.getElementById('patternForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
-    await generatePattern();
-});
 
-window.onload = () => {
-    const isMobile = window.matchMedia("(max-width: 768px)").matches;
-
-    if (!isMobile) {
-        updateModel();
-    } else {
-        document.querySelector('.mobile-message').style.display = 'block';
+    const patternForm = document.getElementById('patternForm');
+    if (patternForm) {
+        patternForm.addEventListener('submit', async function(event) {
+            event.preventDefault();
+            await generatePattern();
+        });
     }
-};
+
+    // Установка начальных значений модели при загрузке страницы
+    window.onload = () => {
+        const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
+        if (!isMobile) {
+            updateModel();
+        } else {
+            document.querySelector('.mobile-message').style.display = 'block';
+        }
+    };
+});
