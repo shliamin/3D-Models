eec040ab2d0f263e1b2448ff6ce1ab28ba42b15f
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-05 12:17:32 +0200

    Update model.js

--- model.js
+++ model.js
@@ -19,10 +19,10 @@ let vertices = [
 ];
 
 // Draw arcs intersecting at the tent's top vertex
-let arc1 = perfectArc(vertices[0], vertices[4], height);
-let arc2 = perfectArc(vertices[1], vertices[3], height);
-let arc3 = perfectArc(vertices[0], vertices[4], height);
-let arc4 = perfectArc(vertices[3], vertices[1], height);
+let arc1 = perfectArc(vertices[0], vertices[3], height);
+let arc2 = perfectArc(vertices[1], vertices[2], height);
+let arc3 = perfectArc(vertices[0], vertices[3], height);
+let arc4 = perfectArc(vertices[2], vertices[1], height);
 
 // Interpolate to create surface points between arcs
 let surface1 = interpolateSurface(arc1, arc2);
