511fa344e0b5225718b95f1b791ba6ba42e6cfa0
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-05 03:14:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,20 @@ import { updateModel } from './model.js';
 import { generateModel } from './server.js';
 import { generatePattern } from './server.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Установка обработчиков событий после полной загрузки DOM
-    document.getElementById('generateModelButton').onclick = generateModel;
-    document.getElementById('generatePatternButton').onclick = generatePattern;
-    document.getElementById('width').onchange = updateModel;
-    document.getElementById('depth').onchange = updateModel;
-    document.getElementById('height').onchange = updateModel;
-    document.getElementById('surface1').onchange = updateModel;
-    document.getElementById('surface2').onchange = updateModel;
+document.getElementById('generateModelButton').onclick = generateModel;
+document.getElementById('generatePatternButton').onclick = generatePattern;
+document.getElementById('width').onchange = updateModel;
+document.getElementById('depth').onchange = updateModel;
+document.getElementById('height').onchange = updateModel;
+document.getElementById('surface1').onchange = updateModel;
+document.getElementById('surface2').onchange = updateModel;
 
+window.onload = () => {
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-    const patternForm = document.getElementById('patternForm');
-    if (patternForm) {
-        patternForm.addEventListener('submit', async function(event) {
-            event.preventDefault();
-            await generatePattern();
-        });
+    if (!isMobile) {
+        updateModel();
+    } else {
+        document.querySelector('.mobile-message').style.display = 'block';
     }
-
-    // Установка начальных значений модели при загрузке страницы
-    window.onload = () => {
-        const isMobile = window.matchMedia("(max-width: 768px)").matches;
-
-        if (!isMobile) {
-            updateModel();
-        } else {
-            document.querySelector('.mobile-message').style.display = 'block';
-        }
-    };
-});
+};
