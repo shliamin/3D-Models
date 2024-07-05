fcd4193e203b8a700b40a810ec377bbed8b0862d
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-05 23:59:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-import { initializeGeoGebra } from './model.js';
+import { updateModel } from './model.js';
 import { generateModel } from './server.js';
 import { generatePattern } from './server.js';
 
 document.getElementById('generateModelButton').onclick = generateModel;
 document.getElementById('generatePatternButton').onclick = generatePattern;
+document.getElementById('width').onchange = updateModel;
+document.getElementById('depth').onchange = updateModel;
+document.getElementById('height').onchange = updateModel;
+document.getElementById('surface1').onchange = updateModel;
+document.getElementById('surface2').onchange = updateModel;
 
 window.onload = () => {
     const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
     if (!isMobile) {
-        initializeGeoGebra();
+        updateModel();
     } else {
         document.querySelector('.mobile-message').style.display = 'block';
     }
