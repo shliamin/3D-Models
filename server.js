48fbe8af00f3a408faec25b71414ac3efe287957
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-06 12:08:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -61,8 +61,8 @@ export async function generateModel() {
     const surface1 = interpolateSurface(arc1, arc2, 100);
     const surface2 = interpolateSurface(arc2, arc3, 100);
 
-    const intersection1 = findIntersection(arc1, arc3, 100)
-    const intersection2 = findIntersection(arc2, arc3, 100)
+    //const intersection1 = findIntersection(arc1, arc3, 100)
+    //const intersection2 = findIntersection(arc2, arc3, 100)
     
     const surface3 = interpolateSurface(arc1, arc2, 100, intersection1);
     const surface4 = interpolateSurface(arc2, arc3, 100, intersection2);
@@ -167,8 +167,8 @@ export async function generatePattern() {
     };
 
     // Interpolate surfaces until intersection
-    const intersection1 = findIntersection(arc1, arc3, 100)
-    const intersection2 = findIntersection(arc2, arc3, 100)
+    //const intersection1 = findIntersection(arc1, arc3, 100)
+    //const intersection2 = findIntersection(arc2, arc3, 100)
     
     const surface3 = interpolateSurface(arc1, arc2, 100, intersection1);
     const surface4 = interpolateSurface(arc2, arc3, 100, intersection2);
