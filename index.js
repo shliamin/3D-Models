2164abb6d17c053c1a6abd335d8f3a13b23e7f4a
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-09 11:00:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,110 +1,135 @@
-import { generateSemiEllipse, generateHalfSemiEllipse, interpolateSurface, calculateDiagonals, linspace, findIntersection } from './model-utils.js';
+import { updateModel } from './model.js';
+import { generateSemiEllipse, interpolateSurface, calculateDiagonals, linspace } from './model-utils.js';
 
-function showSpinner() {
-    document.getElementById('spinner').style.display = 'block';
+document.getElementById('generateModelButton').onclick = () => savePayloadAndRedirect(3);
+document.getElementById('generatePatternButton').onclick = () => savePayloadAndRedirect(10);
+document.getElementById('width').onchange = validateAndUpdateModel;
+document.getElementById('depth').onchange = validateAndUpdateModel;
+document.getElementById('height').onchange = validateAndUpdateModel;
+document.getElementById('surface1').onchange = updateModel; // Surface 1
+document.getElementById('surface2').onchange = updateModel; // Surface 2
+
+window.onload = () => {
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+
+    if (!isMobile) {
+        updateModel();
+    } else {
+        document.querySelector('.mobile-message').style.display = 'block';
+    }
+};
+
+function validateAndUpdateModel(event) {
+    const standardValues = {
+        width: 140,
+        depth: 230,
+        height: 120
+    };
+
+    const target = event.target;
+    const value = parseInt(target.value);
+    let minValue, maxValue;
+
+    switch(target.id) {
+        case 'width':
+            minValue = standardValues.width * 0.1;
+            maxValue = standardValues.width * 2;
+            break;
+        case 'depth':
+            minValue = standardValues.depth * 0.1;
+            maxValue = standardValues.depth * 2;
+            break;
+        case 'height':
+            minValue = standardValues.height * 0.1;
+            maxValue = standardValues.height * 2;
+            break;
+        default:
+            return;
+    }
+
+    if (value < minValue || value > maxValue) {
+        showPopup();
+        target.value = value < minValue ? minValue : maxValue;
+    }
+
+    updateModel();
 }
 
-function hideSpinner() {
-    document.getElementById('spinner').style.display = 'none';
-}
+function showPopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'block';
+    document.body.classList.add('blurred');
 
-async function checkPromoCode() {
-    const promoCode = document.getElementById('promoCode').value;
-    showSpinner();
-    try {
-        const response = await fetch('https://interactive-tent-0697ab02fbe0.herokuapp.com/check_promo_code', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ promo_code: promoCode })
-        });
+    const closeButton = document.getElementById('closePopup');
+    closeButton.onclick = function() {
+        popup.style.display = 'none';
+        document.body.classList.remove('blurred');
+        removeBackgroundBlur();
+    };
 
-        const data = await response.json();
-        hideSpinner();
-        if (data.status === 'success') {
-            handlePaymentWithoutPayPal(data.amount);
-        } else {
-            alert('Invalid promo code.');
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = 'none';
+            document.body.classList.remove('blurred');
+            removeBackgroundBlur();
         }
-    } catch (error) {
-        hideSpinner();
-        console.error('There was an error checking the promo code:', error);
-        alert('There was an error checking the promo code. Please try again.');
+    };
+
+    addBackgroundBlur();
+}
+
+function addBackgroundBlur() {
+    const blurDiv = document.createElement('div');
+    blurDiv.classList.add('background-blur');
+    blurDiv.id = 'backgroundBlur';
+    document.body.appendChild(blurDiv);
+}
+
+function removeBackgroundBlur() {
+    const blurDiv = document.getElementById('backgroundBlur');
+    if (blurDiv) {
+        document.body.removeChild(blurDiv);
     }
 }
 
-function handlePaymentWithoutPayPal(amount) {
-    if (amount == 3) {
-        generateModel();
-    } else if (amount == 10) {
-        generatePattern();
-    }
-}
-
-function redirectToPaypalMe(amount) {
-    showSpinner();
-    let paypalMeLink = `https://www.paypal.me/efimsh/${amount}`;
-    window.location.href = paypalMeLink;
-}
-
-export async function generateModel() {
+function savePayloadAndRedirect(amount) {
     const width = parseFloat(document.getElementById('width').value);
     const depth = parseFloat(document.getElementById('depth').value);
     const height = parseFloat(document.getElementById('height').value);
 
-    console.log('Current values for 3D Model:', { width, depth, height });
-
     const numPoints = 100;
 
-    const { lengths: [diagonal1, diagonal2], endCoordinates: [endCoord1Start, endCoord1End, endCoord2Start, endCoord2End] } = calculateDiagonals(width, depth);
-
-    const semiEllipse1 = generateSemiEllipse(diagonal1 / 2, height, 100);
-    const semiEllipse2 = generateSemiEllipse(diagonal2 / 2, height, 100);
-
+    const { lengths: [diagonal1, diagonal2] } = calculateDiagonals(width, depth);
+    const semiEllipse1 = generateSemiEllipse(diagonal1 / 2, height, numPoints);
+    const semiEllipse2 = generateSemiEllipse(diagonal2 / 2, height, numPoints);
     const x_fine1 = semiEllipse1.x;
     const z_fine1 = semiEllipse1.y;
     const x_fine2 = semiEllipse2.x;
     const z_fine2 = semiEllipse2.y;
-    const y = linspace(0, depth, 100);
-
-    const apex = { x: x_fine1[0], y: y[0], z: z_fine1[0] };
+    const y = linspace(0, depth, numPoints);
 
     const arc1 = {
         x: x_fine1,
         y: y,
-        z: z_fine1,
-        type: 'scatter3d',
-        mode: 'lines',
-        line: { color: 'blue', width: 5 },
-        name: 'Tent Frame 1'
+        z: z_fine1
     };
 
     const arc2 = {
         x: x_fine1.map(x => -x),
         y: y,
-        z: z_fine1,
-        type: 'scatter3d',
-        mode: 'lines',
-        line: { color: 'blue', width: 5 },
-        name: 'Tent Frame 2'
+        z: z_fine1
     };
 
     const arc3 = {
         x: [...arc1.x].reverse(),
         y: [...arc1.y].reverse(),
-        z: [...arc1.z].reverse(),
-        type: 'scatter3d',
-        mode: 'lines',
-        line: { color: 'blue', width: 5 },
-        visible: false
+        z: [...arc1.z].reverse()
     };
 
-    const surface1 = interpolateSurface(arc1, arc2, 100);
-    const surface2 = interpolateSurface(arc2, arc3, 100);
-
-    const surface3 = interpolateSurface(arc1, arc2, 100, 5, true);
-    const surface4 = interpolateSurface(arc2, arc3, 100, 5, true);
+    const surface1 = interpolateSurface(arc1, arc2, numPoints);
+    const surface2 = interpolateSurface(arc2, arc3, numPoints);
+    const surface3 = interpolateSurface(arc1, arc2, numPoints, 5, true);
+    const surface4 = interpolateSurface(arc2, arc3, numPoints, 5, true);
 
     const payload = {
         width,
@@ -117,151 +142,60 @@ export async function generateModel() {
         enable_relaxation: true
     };
 
-    console.log('Sending payload for 3D Model:', payload);
+    fetch('https://interactive-tent-0697ab02fbe0.herokuapp.com/save_payload', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ payload, amount })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.status === 'success') {
+            redirectToPaypalMe(amount);
+        }
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+}
 
-    showSpinner();
+function redirectToPaypalMe(amount) {
+    let paypalMeLink = `https://www.paypal.me/efimsh/${amount}`;
+    window.location.href = paypalMeLink;
+}
+
+async function applyPromoCode() {
+    const promoCode = document.getElementById('promoCode').value;
 
     try {
-        const response = await fetch('https://interactive-tent-0697ab02fbe0.herokuapp.com/generate_model', {
+        const response = await fetch('https://interactive-tent-0697ab02fbe0.herokuapp.com/apply_promo_code', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(payload)
+            body: JSON.stringify({ promo_code: promoCode })
         });
 
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Invalid promo code');
         }
 
-        console.log('3D Model file successfully sent to server.');
-
-        const blob = await response.blob();
-        const link = document.createElement('a');
-        link.href = window.URL.createObjectURL(blob);
-        link.download = 'models.zip';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-    } finally {
-        hideSpinner();
-    }
-}
-
-export async function generatePattern() {
-    const width = parseFloat(document.getElementById('width').value);
-    const depth = parseFloat(document.getElementById('depth').value);
-    const height = parseFloat(document.getElementById('height').value);
-
-    console.log('Current values for 2D Pattern:', { width, depth, height });
-
-    const numPoints = 100;
-
-    const { lengths: [diagonal1, diagonal2] } = calculateDiagonals(width, depth);
-
-    const semiEllipse1 = generateSemiEllipse(diagonal1 / 2, height, 100);
-    const semiEllipse2 = generateSemiEllipse(diagonal2 / 2, height, 100);
-
-    const x_fine1 = semiEllipse1.x;
-    const z_fine1 = semiEllipse1.y;
-    const x_fine2 = semiEllipse2.x;
-    const z_fine2 = semiEllipse2.y;
-    const y = linspace(0, depth, 100);
-
-    const apex = { x: x_fine1[0], y: y[0], z: z_fine1[0] };
-
-    const arc1 = {
-        x: x_fine1,
-        y: y,
-        z: z_fine1,
-        type: 'scatter3d',
-        mode: 'lines',
-        line: { color: 'blue', width: 5 },
-        name: 'Tent Frame 1'
-    };
-
-    const arc2 = {
-        x: x_fine1.map(x => -x),
-        y: y,
-        z: z_fine1,
-        type: 'scatter3d',
-        mode: 'lines',
-        line: { color: 'blue', width: 5 },
-        name: 'Tent Frame 2'
-    };
-
-    const arc3 = {
-        x: [...arc1.x].reverse(),
-        y: [...arc1.y].reverse(),
-        z: [...arc1.z].reverse(),
-        type: 'scatter3d',
-        mode: 'lines',
-        line: { color: 'blue', width: 5 },
-        visible: false
-    };
-
-    const surface3 = interpolateSurface(arc1, arc2, 100, 5, true);
-    const surface4 = interpolateSurface(arc2, arc3, 100, 5, true);
-
-    const payload = {
-        width,
-        depth,
-        height,
-        surface3,
-        surface4,
-        enable_relaxation: true
-    };
-
-    console.log('Sending payload for 2D Pattern:', payload);
-
-    showSpinner();
-
-    try {
-        const response = await fetch('https://interactive-tent-0697ab02fbe0.herokuapp.com/generate_pattern', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        });
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+        const data = await response.json();
+        if (data.valid) {
+            alert('Promo code applied successfully! You can now proceed to download.');
+            // Redirect to appropriate download link or perform any other action
+            window.location.href = data.redirect_url;
+        } else {
+            alert('Invalid promo code');
         }
-
-        console.log('2D Pattern file successfully sent to server.');
-
-        const blob = await response.blob();
-        const link = document.createElement('a');
-        link.href = window.URL.createObjectURL(blob);
-        link.download = 'Patterns_and_Models.zip';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
-    } finally {
-        hideSpinner();
+        alert('Invalid promo code');
     }
 }
-
-// Прикрепляем обработчики событий
-document.getElementById('generateModelButton').onclick = () => {
-    const promoCode = prompt('Enter promo code if you have one, or leave blank for payment.');
-    if (promoCode) {
-        checkPromoCode();
-    } else {
-        redirectToPaypalMe(3);
-    }
-};
-
-document.getElementById('generatePatternButton').onclick = () => {
-    const promoCode = prompt('Enter promo code if you have one, or leave blank for payment.');
-    if (promoCode) {
-        checkPromoCode();
-    } else {
-        redirectToPaypalMe(10);
-    }
-};
