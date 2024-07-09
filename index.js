8dc7d0aec4d82400eb2e4248fcbc1fca38d78db8
Efim <47456474+shliamin@users.noreply.github.com>
2024-07-09 12:51:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import { updateModel } from './model.js';
-import { generateSemiEllipse, interpolateSurface, calculateDiagonals, linspace } from './model-utils.js';
-
 document.getElementById('generateModelButton').onclick = () => savePayloadAndRedirect(3);
 document.getElementById('generatePatternButton').onclick = () => savePayloadAndRedirect(10);
 document.getElementById('width').onchange = validateAndUpdateModel;
@@ -19,6 +16,124 @@ window.onload = () => {
         document.querySelector('.mobile-message').style.display = 'block';
     }
 };
+
+async function savePayloadAndRedirect(amount) {
+    showSpinner();
+
+    const payload = getPayload();
+    
+    try {
+        const response = await fetch('https://interactive-tent-0697ab02fbe0.herokuapp.com/save_payload', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ payload, amount })
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        if (data.status === 'success') {
+            redirectToPaypalMe(amount);
+        }
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    } finally {
+        hideSpinner();
+    }
+}
+
+async function applyPromoCode() {
+    showSpinner();
+
+    const promoCode = document.getElementById('promoCode').value;
+    const payload = getPayload();
+
+    try {
+        const response = await fetch('https://interactive-tent-0697ab02fbe0.herokuapp.com/promo/check_promo_code', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ promo_code: promoCode, payload })
+        });
+
+        if (!response.ok) {
+            throw new Error('Invalid promo code');
+        }
+
+        const data = await response.json();
+        if (data.status === 'success') {
+            window.location.href = data.redirect_url;
+        } else {
+            alert('Invalid promo code');
+        }
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        alert('Invalid promo code');
+    } finally {
+        hideSpinner();
+    }
+}
+
+function getPayload() {
+    const width = parseFloat(document.getElementById('width').value);
+    const depth = parseFloat(document.getElementById('depth').value);
+    const height = parseFloat(document.getElementById('height').value);
+
+    const numPoints = 100;
+
+    const { lengths: [diagonal1, diagonal2] } = calculateDiagonals(width, depth);
+    const semiEllipse1 = generateSemiEllipse(diagonal1 / 2, height, numPoints);
+    const semiEllipse2 = generateSemiEllipse(diagonal2 / 2, height, numPoints);
+    const x_fine1 = semiEllipse1.x;
+    const z_fine1 = semiEllipse1.y;
+    const x_fine2 = semiEllipse2.x;
+    const z_fine2 = semiEllipse2.y;
+    const y = linspace(0, depth, numPoints);
+
+    const arc1 = {
+        x: x_fine1,
+        y: y,
+        z: z_fine1
+    };
+
+    const arc2 = {
+        x: x_fine1.map(x => -x),
+        y: y,
+        z: z_fine1
+    };
+
+    const arc3 = {
+        x: [...arc1.x].reverse(),
+        y: [...arc1.y].reverse(),
+        z: [...arc1.z].reverse()
+    };
+
+    const surface1 = interpolateSurface(arc1, arc2, numPoints);
+    const surface2 = interpolateSurface(arc2, arc3, numPoints);
+    const surface3 = interpolateSurface(arc1, arc2, numPoints, 5, true);
+    const surface4 = interpolateSurface(arc2, arc3, numPoints, 5, true);
+
+    return {
+        width,
+        depth,
+        height,
+        surface1,
+        surface2,
+        surface3,
+        surface4,
+        enable_relaxation: true
+    };
+}
+
+function redirectToPaypalMe(amount) {
+    let paypalMeLink = `https://www.paypal.me/efimsh/${amount}`;
+    window.location.href = paypalMeLink;
+}
 
 function validateAndUpdateModel(event) {
     const standardValues = {
@@ -99,124 +214,4 @@ function showSpinner() {
 
 function hideSpinner() {
     document.getElementById('spinner').style.display = 'none';
-}
-
-function savePayloadAndRedirect(amount) {
-    showSpinner();
-
-    const payload = getPayload();
-    
-    fetch('https://interactive-tent-0697ab02fbe0.herokuapp.com/save_payload', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ payload, amount })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        if (data.status === 'success') {
-            redirectToPaypalMe(amount);
-        }
-    })
-    .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-    })
-    .finally(() => {
-        hideSpinner();
-    });
-}
-
-function getPayload() {
-    const width = parseFloat(document.getElementById('width').value);
-    const depth = parseFloat(document.getElementById('depth').value);
-    const height = parseFloat(document.getElementById('height').value);
-
-    const numPoints = 100;
-
-    const { lengths: [diagonal1, diagonal2] } = calculateDiagonals(width, depth);
-    const semiEllipse1 = generateSemiEllipse(diagonal1 / 2, height, numPoints);
-    const semiEllipse2 = generateSemiEllipse(diagonal2 / 2, height, numPoints);
-    const x_fine1 = semiEllipse1.x;
-    const z_fine1 = semiEllipse1.y;
-    const x_fine2 = semiEllipse2.x;
-    const z_fine2 = semiEllipse2.y;
-    const y = linspace(0, depth, numPoints);
-
-    const arc1 = {
-        x: x_fine1,
-        y: y,
-        z: z_fine1
-    };
-
-    const arc2 = {
-        x: x_fine1.map(x => -x),
-        y: y,
-        z: z_fine1
-    };
-
-    const arc3 = {
-        x: [...arc1.x].reverse(),
-        y: [...arc1.y].reverse(),
-        z: [...arc1.z].reverse()
-    };
-
-    const surface1 = interpolateSurface(arc1, arc2, numPoints);
-    const surface2 = interpolateSurface(arc2, arc3, numPoints);
-    const surface3 = interpolateSurface(arc1, arc2, numPoints, 5, true);
-    const surface4 = interpolateSurface(arc2, arc3, numPoints, 5, true);
-
-    return {
-        width,
-        depth,
-        height,
-        surface1,
-        surface2,
-        surface3,
-        surface4,
-        enable_relaxation: true
-    };
-}
-
-function redirectToPaypalMe(amount) {
-    let paypalMeLink = `https://www.paypal.me/efimsh/${amount}`;
-    window.location.href = paypalMeLink;
-}
-
-async function applyPromoCode() {
-    showSpinner();
-
-    const promoCode = document.getElementById('promoCode').value;
-    const payload = getPayload();
-
-    try {
-        const response = await fetch('https://interactive-tent-0697ab02fbe0.herokuapp.com/promo/check_promo_code', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ promo_code: promoCode, payload })
-        });
-
-        if (!response.ok) {
-            throw new Error('Invalid promo code');
-        }
-
-        const data = await response.json();
-        if (data.status === 'success') {
-            window.location.href = data.redirect_url;
-        } else {
-            alert('Invalid promo code');
-        }
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-        alert('Invalid promo code');
-    } finally {
-        hideSpinner();
-    }
 }
